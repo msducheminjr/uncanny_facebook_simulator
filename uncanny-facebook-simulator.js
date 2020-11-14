14d8523ebe61a17673dfea84ba0a0ea7cc3979ab
Michael Duchemin <msducheminjr@users.noreply.github.com>
2020-11-14 11:16:15 -0500

    Modify frequencyModifier and calcInterval
    Perform some refactoring and redesign to make the addToChaos() function
    easier to use when adding new messages.
    
    * Modify the frequencyModifier of the addToChaos() function to be in
      seconds instead of milliseconds
    * Change the calcInterval() function to use a while loop and multiply by 1.25
      instead of a one-time multiplication by 10
    * Add commenting to the addToChaos function to explain usage when adding
      new messages

--- uncanny-facebook-simulator.js
+++ uncanny-facebook-simulator.js
@@ -1,16 +1,30 @@
 // https://github.com/msducheminjr/node_getting_started/1-getting-started/2-timers/uncanny-facebook-simulator.js
 const calcInterval = function(multiplier) {
+  multiplier = multiplier * 1000;
   result = Math.random() * multiplier;
-  if (result < 2000) {
-    result *= 10;
+  while (result < 4000) {
+    result *= 1.25;
   }
   return result;
 };
 
-const addToChaos = function(fbMessage, frequency) {
+// adding a new message
+// addToChaos(String fbMessage, Integer frequencyModifier);
+/* fbMessage
+ * Be mindful that the typical Linux terminal wraps at 80 characters and will split you up
+ * in the middle of a word. Try to keep messages under 80 characters. If you need a second
+ * line to elaborate on a message, use the \n character to break across multiple lines\.
+ *
+ * frequencyModifier
+ * The interval of each post is random, but the frequencyModifier provides the ability to
+ * increase or decrease the relative frequency of messages over multiple executions of the
+ * simulator. The multiplier is equivalent to the maximum number of seconds if Math.random()
+ * came up with a number like .999
+*/
+const addToChaos = function(fbMessage, frequencyModifier) {
   setInterval(
     () => console.log(fbMessage),
-    calcInterval(frequency)
+    calcInterval(frequencyModifier)
   );
 }
 
@@ -23,51 +37,51 @@ setInterval(
 addToChaos(
   'Partially false information:\n' +
   '(Checked by independent fact checkers who agree with Facebook about everything)',
-  51000
+  51
 );
 
-addToChaos('Copy and paste (do not share) my digital chain letter!', 48750);
+addToChaos('Copy and paste (do not share) my digital chain letter!', 48);
 
-addToChaos("I bet most people won’t repost this. You know who you are.", 53336);
+addToChaos("I bet most people won’t repost this. You know who you are.", 53);
 
-addToChaos('TRUMP IS LITERALLY HITLER!', 56890);
+addToChaos('TRUMP IS LITERALLY HITLER!', 56);
 
-addToChaos('TRUMP IS LITERALLY JESUS!', 79880);
+addToChaos('TRUMP IS LITERALLY JESUS!', 79);
 
-addToChaos('Questionnaire phishing for password reset questions (always set to public)', 57880);
+addToChaos('Questionnaire phishing for password reset questions (always set to public)', 57);
 
-addToChaos('Ad that knows me creepily well', 81000);
+addToChaos('Ad that knows me creepily well', 81);
 
-addToChaos('Ad that has no clue about me.', 84000);
+addToChaos('Ad that has no clue about me.', 84);
 
-addToChaos('Old person sharing missing person case that was closed years ago.', 96000);
+addToChaos('Old person sharing missing person case that was closed years ago.', 96);
 
-addToChaos('(Heated political argument between friends)', 110000);
+addToChaos('(Heated political argument between friends)', 110);
 
-addToChaos('[[ARGUING OVER THE INTERNET INTENSIFIES]]', 140000);
+addToChaos('[[ARGUING OVER THE INTERNET INTENSIFIES]]', 140);
 
-addToChaos('Funny video', 160000);
+addToChaos('Funny video', 160);
 
-addToChaos('Lame meme', 112030);
+addToChaos('Lame meme', 112);
 
-addToChaos('Actually funny meme', 221070);
+addToChaos('Actually funny meme', 221);
 
-addToChaos('Post about somebody\'s life. Why I joined in the first place...', 139770);
+addToChaos('Post about somebody\'s life. Why I joined in the first place...', 139);
 
-addToChaos('Cat video', 121070);
+addToChaos('Cat video', 121);
 
-addToChaos('Unsolicited friend request', 13888);
+addToChaos('Unsolicited friend request', 138);
 
-addToChaos('I can almost feel Zuckerberg breathing on me', 67002);
+addToChaos('I can almost feel Zuckerberg breathing on me', 108);
 
-addToChaos('[[TRIGGERED OPINIONATED SCREECHING INTENSIFIES]]', 111112);
+addToChaos('[[TRIGGERED OPINIONATED SCREECHING INTENSIFIES]]', 180);
 
-addToChaos('Food picture', 114322);
+addToChaos('Food picture', 114);
 
-addToChaos('Spoiler for the game I was recording', 87654);
+addToChaos('Spoiler for the game I was recording', 87);
 
 addToChaos(
   "I'm leaving Facebook for good. Join me on [name of new platform]!\n" +
   "(I'll be back in a month...)",
-  114322
+  114
 );
